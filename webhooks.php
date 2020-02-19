bae73b8fb460daa4e7e6320828239e88f1fc4880
Madheswaran <kmadheswaran@paypal.com>
2020-02-19 06:36:31 +0530

    modified

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,11 @@
 <?php
 $rawData = file_get_contents("php://input");
 $textfilename="text.txt";
-$phno = 1;
-file_put_contents ($textfilename, $phno."\n".$rawData."\n");
+$success = file_put_contents($textfilename, $rawData."\n");
+if ($success === TRUE)
+{
+	echo "success";
+} else {
+	echo "faield";
+}
 ?>
