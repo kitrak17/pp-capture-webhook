16360e7fde93db7fcbc476c2bf41ef3c67f5dad9
Madheswaran <kmadheswaran@paypal.com>
2020-02-19 06:33:49 +0530

    modified

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,6 @@
 <?php
 $rawData = file_get_contents("php://input");
-$textfilename="text ".date("Y-m-d His").".txt";
+$textfilename="text.txt";
 $phno = 1;
 file_put_contents ($textfilename, $phno."\n".$rawData."\n");
 ?>
