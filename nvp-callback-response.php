0a11eb638e03e64cbd1b9f3d90a4da4925635e3c
Karthick <mkartik17@gmail.com>
2021-11-17 13:27:36 +0530

    Update nvp-callback-response.php

--- nvp-callback-response.php
+++ nvp-callback-response.php
@@ -1,1 +1,6 @@
-
+<?php
+$rawData = file_get_contents("php://input");
+$timstamp = "\n\n------------------------------------------------------------------------------------------------\n".date("Y-m-d H:i:s a")."\n------------------------------------------------------------------------------------------------\n\n";
+$success = file_put_contents('logs.txt', $timstamp."\n".$rawData.PHP_EOL , FILE_APPEND | LOCK_EX);
+echo 'METHOD=CallbackResponse&NO_SHIPPING_OPTION_DETAILS=1&CALLBACKVERSION=61';
+?>
