8f6e10be1ed26e220238128bc709cffe64a5f5f5
Karthick <mkartik17@gmail.com>
2021-11-25 09:35:25 +0530

    Update bt-read-webhook.php

--- bt-read-webhook.php
+++ bt-read-webhook.php
@@ -13,7 +13,7 @@ $gateway = new Braintree_Gateway([
 try {
 
     $webhookNotification = $gateway->webhookNotification()->parse(
-        "8qvhrwz6fm24dym2%7C48e018ca371559f16b40ca6b855ce5aefdfd3ca7%268wpp4nqmb9bdtkqx%7C0769f4ba7806e5dd906aa1f97ab901c1485f8924", "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPG5vdGlm%0AaWNhdGlvbj4KICA8a2luZD5jaGVjazwva2luZD4KICA8dGltZXN0YW1wIHR5%0AcGU9ImRhdGV0aW1lIj4yMDIxLTExLTI1VDAzOjQ3OjU3WjwvdGltZXN0YW1w%0APgogIDxzdWJqZWN0PgogICAgPGNoZWNrIHR5cGU9ImJvb2xlYW4iPnRydWU8%0AL2NoZWNrPgogIDwvc3ViamVjdD4KPC9ub3RpZmljYXRpb24%2BCg%3D%3D%0A"
+        $_POST["bt_signature"], $_POST["bt_payload"]
     );
 
     // Example values for webhook notification properties
